ee5975263ebad271ffebb4995745630edbe6be93
Arnab Bose <hirak99@gmail.com>
2025-10-08 21:29:14 +0530

    Change streamlit port

--- run_ui_demo.sh
+++ run_ui_demo.sh
@@ -5,5 +5,5 @@ set -uexo pipefail
 readonly MY_PATH="$(dirname "$(realpath "$0")")"
 cd "$MY_PATH"/src
 
-streamlit run gui_play.py
-
+# Default port is first free port, starting at 8501.
+streamlit run gui_play.py --server.port 8009
